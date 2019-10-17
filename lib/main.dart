0b66ff140e4b54c134a5af4aa2907834f65ae175
Matheus <matheus.carvalho.sales@gmail.com>
2019-10-17 20:56:05 -0300

    animação

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
-import 'package:alcool_ou_gasolina/widgets/loading-button.widget.dart';
-import 'package:alcool_ou_gasolina/widgets/logo.widget.dart';
-import 'package:alcool_ou_gasolina/widgets/submit-form.dart';
-import 'package:alcool_ou_gasolina/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
-
-import 'widgets/input.widget.dart';
 import 'widgets/pages/home.page.dart';
 
 void main() => runApp(MyApp());
