45a1686318da3d75acd7ff14073bd7256a0439ae
Matheus <matheus.carvalho.sales@gmail.com>
2019-10-17 20:54:57 -0300

    animação de transição

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:alcool_ou_gasolina/widgets/loading-button.widget.dart';
 import 'package:alcool_ou_gasolina/widgets/logo.widget.dart';
+import 'package:alcool_ou_gasolina/widgets/submit-form.dart';
 import 'package:alcool_ou_gasolina/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 import 'widgets/input.widget.dart';
+import 'widgets/pages/home.page.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,41 +20,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  var _gasCtrl = new MoneyMaskedTextController();
-  var _alcCtrl = new MoneyMaskedTextController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: ListView(
-        children: <Widget>[
-          Logo(),
-          // Success(
-          //   reset: () {},
-          //   result: "Compensa utilizar X",
-          // ),
-          Input(
-            ctrl: _gasCtrl,
-            label: "Gasolina",
-          ),
-          Input(
-            ctrl: _alcCtrl,
-            label: "Álcool",
-          ),
-          LoadingButton(
-            busy: true,
-            func: () {},
-            text: "CALCULAR",
-            invert: false,
-          ),
-        ],
-      ),
     );
   }
 }
